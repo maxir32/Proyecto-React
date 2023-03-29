70b7112009583416e9444eb606c7535a98b12034
maxir32 <maxi.rozo22@gmail.com>
2023-03-28 21:20:17 -0300

    Preentrega1 Rozo

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,16 @@
+import CartWidget from "../CartWidget/CartWidget";
+
+const NavBar = () => {
+    return(
+        <nav>
+            <h3>KALEM</h3>
+            <div>
+                <button>Diplomaturas</button>
+                <button>Grupos</button>
+                <button>Talleres</button>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar;
